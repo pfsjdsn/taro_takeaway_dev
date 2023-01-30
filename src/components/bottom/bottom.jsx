09edf746d5df2ffdd029ee86c791fe89677130b8
pfsjdsn <canoecanoe@aliyun.com>
2023-01-30 09:57:06 +0800

    flex 布局2

--- src/components/bottom/bottom.jsx
+++ src/components/bottom/bottom.jsx
@@ -4,18 +4,27 @@ import './bottom.less'
 class Bottom extends Component {
 	constructor() {
 		super(...arguments);
-		this.state = {};
+		this.state = {
+			Num: 0,
+			// 配送费
+			sendPrice: 4,
+			supportTakeBySelf: false,
+			// 起送费
+			sendMustPrice: 20
+		};
 	}
 	render () {
+		let { Num, sendPrice, supportTakeBySelf, sendMustPrice } = this.state
 		return (<View className='bottom'>
 			<View className='bottom_body'>
+				{Num ? <Text className='num'>{Num}</Text> : null}
 				<img className='store_img' src={require('../../assets/img/emptystore.png')} />
 				<View className='info'>
-					<Text>另需配送费￥3</Text>
-					<Text>支持自取</Text>
+					<Text>{sendPrice ? '另需配送费￥ |' + sendPrice : ''}</Text>
+					<Text>{supportTakeBySelf ? '支持自取' : '不支持自取'}</Text>
 				</View>
 				<View className='submit'>
-					<Text>￥3起送</Text>
+					<Text>{sendMustPrice ? '￥' + sendMustPrice + '起送' : ''}</Text>
 				</View>
 			</View>
 
